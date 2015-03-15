e2826f28ad8a5a0bf221fcef9f96832637da69e2
Parvu Leonard <parvu.leonard@yahoo.com>
2015-03-15 14:45:03 +0200

    message added

--- FirstAssignment/src/firstassignment/FirstAssignment.java
+++ FirstAssignment/src/firstassignment/FirstAssignment.java
@@ -18,6 +18,7 @@ public class FirstAssignment {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("How are you today?");
     }
     
 }
